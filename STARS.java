2ee39aca6445e62821a2153e59aade2080c9089d
limsiyi <73398613+limsiyi@users.noreply.github.com>
2020-11-06 00:50:17 +0800

    Update STARS.java

--- STARS.java
+++ STARS.java
@@ -1,1 +1,24 @@
+import java.util.ArrayList;
+
+public class STARS {
+	
+	
+	private ArrayList<String> Student = new ArrayList<String>();
+
+	private ArrayList<String> Admin = new ArrayList<String>();
+	
+	private ArrayList<String> Course = new ArrayList<String>();
+	
+	private long studentAccessPeriod;
+	
+	private long studentAccessDuration;
+	
+	public Boolean checkVacancies() {
+		
+	}
+	
+	public void showErrorMessage() {
+		System.out.println("Error! Please input again!");
+	}
+}
 
