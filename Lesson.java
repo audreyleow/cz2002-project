16e9788435e3406e8d06633b96db3b415118ad1e
sohwaileong <73398523+sohwaileong@users.noreply.github.com>
2020-11-07 15:52:46 +0800

    Update Lesson.java

--- Lesson.java
+++ Lesson.java
@@ -1,1 +1,10 @@
 
+import java.util.ArrayList;
+
+public class Lesson {
+	private LessonType lessonType;
+	private String classVenue;
+	private ArrayList<Day> classDay;
+	private ArrayList<int[]> classTiming;
+
+}
