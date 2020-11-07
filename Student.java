a31237d631dc9cc76194274142f0a83a6d242165
sohwaileong <73398523+sohwaileong@users.noreply.github.com>
2020-11-07 15:50:31 +0800

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,1 +1,38 @@
 
+public class Student extends User{
+	
+	private String matricNo;
+	private String email;
+	private StudentRecords studentRecords;
+	
+	public Student(String userName, String pwd, String name, String gender, String nationality, boolean accesslevel, String matricNo, String email, StudentRecords studentRecords) {
+		super(userName, pwd, name, gender, nationality, accesslevel);
+		this.matricNo = matricNo;
+		this.email = email;
+		this.studentRecords = studentRecords;
+	}
+
+	public String getMatricNo() {
+		return matricNo;
+	}
+	
+	public void setMatricNo(String matricNo) {
+		this.matricNo = matricNo;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public StudentRecords getStudentRecords() {
+		return studentRecords;
+	}
+	
+	public void setStudentRecords(StudentRecords studentRecords) {
+		this.studentRecords = studentRecords;
+	}
+}
