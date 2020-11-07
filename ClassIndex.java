758517415471e66ed26334c3918cd78f9af8af9d
sohwaileong <73398523+sohwaileong@users.noreply.github.com>
2020-11-07 15:52:17 +0800

    Update ClassIndex.java

--- ClassIndex.java
+++ ClassIndex.java
@@ -1,1 +1,75 @@
+import java.util.ArrayList;
 
+public class ClassIndex {
+	private String courseName;
+	private String courseCode;
+	private int indexNum;
+	private int classSize;
+	private int classVacancy;
+	private ArrayList<Lesson> lessonsList; 
+	private ArrayList<Student> waitList;
+	private ArrayList<Student> studentList;
+	
+	
+	public ClassIndex(String courseName, String courseCode, int indexNum, int classSize, int classVacancy, ArrayList<Lesson> lessonsList, ArrayList<Student> waitList, ArrayList<Student> studentList) {
+		this.courseName = courseName;
+		this.courseCode = courseCode;
+		this.indexNum = indexNum;
+		this.classSize = classSize;
+		this.classVacancy = classVacancy;
+		this.lessonsList = lessonsList;
+		this.waitList = waitList;
+		this.studentList = studentList;
+	}
+	
+	public String getCourseCode() {
+		return courseCode;
+	}
+
+	public void setCourseCode(String courseCode) {
+		this.courseCode = courseCode;
+	}
+
+	public String getCourseName() {
+		return courseName;
+	}
+	public int getIndexNum() {
+		return indexNum;
+	}
+	public int getClassSize() {
+		return classSize;
+	}
+	public int getClassVacancy() {
+		return classVacancy;
+	}
+	public ArrayList<Lesson> getLessonsList() {
+		return lessonsList;
+	}
+	public ArrayList<Student> getWaitList() {
+		return waitList;
+	}
+	public ArrayList<Student> getStudentList() {
+		return studentList;
+	}
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+	public void setIndexNum(int indexNum) {
+		this.indexNum = indexNum;
+	}
+	public void setClassSize(int classSize) {
+		this.classSize = classSize;
+	}
+	public void setClassVacancy(int classVacancy) {
+		this.classVacancy = classVacancy;
+	}
+	public void setLessonsList(ArrayList<Lesson> lessonsList) {
+		this.lessonsList = lessonsList;
+	}
+	public void setWaitList(ArrayList<Student> waitList) {
+		this.waitList = waitList;
+	}
+	public void setStudentList(ArrayList<Student> studentList) {
+		this.studentList = studentList;
+	}
+}
