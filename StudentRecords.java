ad69be3b796308b8c8029ebde69bbd0551c1d01e
sohwaileong <73398523+sohwaileong@users.noreply.github.com>
2020-11-07 15:51:01 +0800

    Update StudentRecords.java

--- StudentRecords.java
+++ StudentRecords.java
@@ -1,1 +1,39 @@
+import java.util.ArrayList;
 
+public class StudentRecords {
+
+	private int acadUnitsRegistered;
+	private ArrayList<ClassIndex> coursesRegistered;
+	
+	public StudentRecords() {
+		acadUnitsRegistered = 0;
+		coursesRegistered = new ArrayList<ClassIndex>();
+	}
+	
+	public void addCourseRegistered(ClassIndex newCourseIndex) {
+		coursesRegistered.add(newCourseIndex);
+	}
+	
+	public void removeCourseRegistered(ClassIndex removedCourseIndex) {
+		if (coursesRegistered.contains(removedCourseIndex)) {
+			coursesRegistered.remove(removedCourseIndex);
+		}
+	}
+
+	public void setAcadUnitsRegistered(int acadUnitsRegistered) {
+		this.acadUnitsRegistered = acadUnitsRegistered;
+	}
+
+	public void setCoursesRegistered(ArrayList<ClassIndex> coursesRegistered) {
+		this.coursesRegistered = coursesRegistered;
+	}
+
+	public int getAcadUnitsRegistered() {
+		return acadUnitsRegistered;
+	}
+
+	public ArrayList<ClassIndex> getCoursesRegistered() {
+		return coursesRegistered;
+	}
+	
+}
