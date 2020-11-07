c71ce0a618bad7d850ffd146ffa0378c52afb361
sohwaileong <73398523+sohwaileong@users.noreply.github.com>
2020-11-07 15:49:51 +0800

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,1 +1,21 @@
 
+public class Admin extends User{
+	private String staffID;
+
+	public Admin(String userName, String pwd, String name, String gender, String nationality, boolean accesslevel, String staffID) {
+		super(userName, pwd, name, gender, nationality, accesslevel);
+		this.staffID = staffID;
+	}
+	
+	public String getStaffID() {
+		return staffID;
+	}
+
+	public void setStaffID(String staffID) {
+		this.staffID = staffID;
+	}
+	
+	
+
+}
+
