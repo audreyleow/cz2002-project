ccb7a42e96183b29c70249aa1493d1f988c4e693
Audrey <audreyleow@yahoo.com.sg>
2020-11-13 03:25:30 +0800

    Create HashPassword.java

---
+++ HashPassword.java
@@ -0,0 +1,24 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class HashPassword {
+
+	public static String generateHash(String data) throws NoSuchAlgorithmException{
+		MessageDigest digest = MessageDigest.getInstance("SHA-256");
+		digest.reset();
+		byte[] hash = digest.digest(data.getBytes());
+		return bytesToStringHex(hash);
+	}
+  
+	private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
+	
+	public static String bytesToStringHex(byte[] bytes) {
+		char[] hexChars = new char[bytes.length * 2];
+		for(int j = 0; j < bytes.length; j++) {
+			int v = bytes[j] & 0xFF;
+			hexChars[j * 2] = hexArray[v >>> 4];
+			hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+		}
+		return new String(hexChars);
+	}
+}
