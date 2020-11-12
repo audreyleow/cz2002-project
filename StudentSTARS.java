97847132717b17d4e0eb64a7b4cc02cbd7856ba7
Audrey <audreyleow@yahoo.com.sg>
2020-11-13 03:30:19 +0800

    Update StudentSTARS.java

--- StudentSTARS.java
+++ StudentSTARS.java
@@ -4,122 +4,127 @@ public class StudentStars extends STARS { //student page
 	Scanner scan = new Scanner(System.in);
   
 	public static void run (Student studentLoggedIn)
-	{
-		System.out.println("================ STARS Menu ================\r\n" + 
-				   "|1. Add Course                             |\r\n" + 
-				   "|2. Drop Course                            |\r\n" + 
-				   "|3. Check Courses Registered               |\r\n" +
-				   "|4. Check Vacancies Available              |\r\n" + 
-				   "|5. Change Index Number of Course          |\r\n" + 
-				   "|6. Swop Index Number with Another Student |\r\n" + 
-				   "|7. Log Out                                |\r\n" + 
-				   "============================================\r\n");
-		int inputChoice;
-		do {
-			System.out.print ("\nWhat do you wish to do?");
-			inputChoice = scan.nextInt();
-			switch(inputChoice) {
-				case 1: // Add Course
-					int indexNumberTemp;
-					do {
-						System.out.print("Input the index number of the course you would like to add:");
-						indexNumberTemp = scan.nextInt();
-						if(indexNumberTemp <= 0) {
-							System.out.println("Please insert a positive integer");
-						}
-					}while(indexNumberTemp <= 0);
-						if (verifyClassIndex(indexNumberTemp) == false) {
-							System.out.println("This index number does not exist.");
-						}
-						else {
-							//addCourse(indexNumberTemp);
-							System.out.println("Your course has been added!");
-						}	
-					break;
-				case 2: // Drop Course
-					int indexNumberTemp2;
-					do {
-						System.out.print("Input the index number of the course you would like to drop:");
-						indexNumberTemp2 = scan.nextInt();
-						if(indexNumberTemp2 <= 0) {
-							System.out.println("Please insert a positive integer");
-						}
-					}while(indexNumberTemp2 <= 0);
-						if (verifyClassIndex(indexNumberTemp2) == false) {
-							System.out.println("This index number does not exist.");
-						}
-						else {
-							//dropCourse(indexNumberTemp2);
-							System.out.println("Your course has been dropped!");
-						}	
-					break;
-				case 3: // Check Courses Registered
-					//printCourses(studentLoggedIn);
-					break;
-				case 4: // Check Vacancies Available
-					int indexNumberTemp3;
-					do {
-						System.out.print("Input index number to check class vacancy:");
-						indexNumberTemp3 = scan.nextInt();
-						if(indexNumberTemp3<=0) {
-							System.out.println("Please insert a positive value");
-						}
-					}while(indexNumberTemp3<=0);
-					//checkVacancies(indexNumberTemp3);
-					break;
-				case 5: // Change Index Number of Course
-					int indexNumberTemp4,indexNumberTemp5;
-					do {
-						System.out.print("Input your index number you wish to change:");
-						indexNumberTemp4 = scan.nextInt();
-						if(indexNumberTemp4 <= 0) {
-							System.out.println("Please insert a positive integer");
-						}
-					}while(indexNumberTemp4 <= 0);
-					do {
-						System.out.print("Input your new index number:");
-						indexNumberTemp5 = scan.nextInt();
-						if(indexNumberTemp5 <= 0) {
-							System.out.println("Please insert a positive integer");
-						}
-					}while(indexNumberTemp5 <= 0);
-					//changeIndex(indexNumberTemp4, indexNumberTemp5);
-					break;
-				case 6: // Swap Index Number with Another Student
-					int indexNumberTemp6,peerIndexNumberTemp;
-					String peerUserName, peerPassword;
-					do {
-						System.out.print("Input your index number you wish to change:");
-						indexNumberTemp6 = scan.nextInt();
-						if(indexNumberTemp6<=0) {
-							System.out.println("Please insert a positive value");
-						}
-					}while(indexNumberTemp6<=0);
-					System.out.print("Input your peer's username:");
-					peerUserName = scan.nextLine();
-					// IS THERE CHECK FOR STRING INPUT? Remove whitespace?
-					System.out.print("Input your peer's username:");
-					peerPassword = scan.nextLine();
-					// IS THERE CHECK FOR STRING INPUT? Remove whitespace?
-					do {
-						System.out.print("Input your index number you wish to change:");
-						indexNumberTemp6 = scan.nextInt();
-						if(indexNumberTemp6<=0) {
-							System.out.println("Please insert a positive value");
-						}
-					}while(indexNumberTemp6<=0);
-					//swopIndex(indexNumberTemp6, peerUserName, peerPassword, peerIndexNumberTemp);
-					break;
-				case 7: // Exit
-					// Call method to (re)write files
-					System.out.println("You have logged out.");
-					break;
-				default:
-					System.out.println("Please insert a positive integer from 1-7");
-			}
-		}while(inputChoice != 7);
-
-	}
+		{
+			System.out.println("================ STARS Menu ================\r\n" + 
+					   "|1. Add Course                             |\r\n" + 
+					   "|2. Drop Course                            |\r\n" + 
+					   "|3. Check Courses Registered               |\r\n" +
+					   "|4. Check Vacancies Available              |\r\n" + 
+					   "|5. Change Index Number of Course          |\r\n" + 
+					   "|6. Swop Index Number with Another Student |\r\n" + 
+					   "|7. Log Out                                |\r\n" + 
+					   "============================================\r\n");
+			int inputChoice;
+			Scanner scan = new Scanner(System.in);
+			do {
+				System.out.print ("\nWhat do you wish to do?");
+				inputChoice = scan.nextInt();
+				switch(inputChoice) {
+					case 1: // Add Course
+						int indexNumberTemp;
+						do {
+							System.out.print("Input the index number of the course you would like to add:");
+							indexNumberTemp = scan.nextInt();
+							if(indexNumberTemp <= 0) {
+								System.out.println("Please insert a positive integer");
+							}
+						}while(indexNumberTemp <= 0);
+							if (verifyClassIndex(indexNumberTemp) == false) {
+								System.out.println("This index number does not exist.");
+							}
+							else {
+								//addCourse(indexNumberTemp);
+								System.out.println("Your course has been added!");
+							}	
+						break;
+					case 2: // Drop Course
+						int indexNumberTemp2;
+						do {
+							System.out.print("Input the index number of the course you would like to drop:");
+							indexNumberTemp2 = scan.nextInt();
+							if(indexNumberTemp2 <= 0) {
+								System.out.println("Please insert a positive integer");
+							}
+						}while(indexNumberTemp2 <= 0);
+							if (verifyClassIndex(indexNumberTemp2) == false) {
+								System.out.println("This index number does not exist.");
+							}
+							else {
+								//dropCourse(indexNumberTemp2);
+								System.out.println("Your course has been dropped!");
+							}	
+						break;
+					case 3: // Check Courses Registered
+						//printCourses(studentLoggedIn);
+						break;
+					case 4: // Check Vacancies Available
+						int indexNumberTemp3;
+						do {
+							System.out.print("Input index number to check class vacancy:");
+							indexNumberTemp3 = scan.nextInt();
+							if(indexNumberTemp3<=0) {
+								System.out.println("Please insert a positive value");
+							}
+						}while(indexNumberTemp3<=0);
+						//checkVacancies(indexNumberTemp3);
+						break;
+					case 5: // Change Index Number of Course
+						int indexNumberTemp4,indexNumberTemp5;
+						do {
+							System.out.print("Input your index number you wish to change:");
+							indexNumberTemp4 = scan.nextInt();
+							if(indexNumberTemp4 <= 0) {
+								System.out.println("Please insert a positive integer");
+							}
+						}while(indexNumberTemp4 <= 0);
+						do {
+							System.out.print("Input your new index number:");
+							indexNumberTemp5 = scan.nextInt();
+							if(indexNumberTemp5 <= 0) {
+								System.out.println("Please insert a positive integer");
+							}
+						}while(indexNumberTemp5 <= 0);
+						//changeIndex(indexNumberTemp4, indexNumberTemp5);
+						break;
+					case 6: // Swap Index Number with Another Student
+						int indexNumberTemp6,peerIndexNumberTemp;
+						String peerUserName, peerPassword, peerHashedPassword;
+						do {
+							System.out.print("Input your index number you wish to change:");
+							indexNumberTemp6 = scan.nextInt();
+							if(indexNumberTemp6<=0) {
+								System.out.println("Please insert a positive value");
+							}
+						}while(indexNumberTemp6<=0);
+						scan.nextLine();
+						System.out.print("Input your peer's username:");
+						peerUserName = scan.nextLine();
+						peerUserName = peerUserName.trim();									// Remove any whitespace at both ends of String
+						peerUserName = peerUserName.replaceAll("\\s+","");					// Remove any whitespace in String
+						System.out.print("Input your peer's password:");
+						peerPassword = scan.nextLine();
+						peerPassword = peerPassword.trim();									// Remove any whitespace at both ends of String
+						peerPassword = peerPassword.replaceAll("\\s+","");					// Remove any whitespace in String
+						peerHashedPassword = HashPassword.generateHash(peerPassword);
+						do {
+							System.out.print("Input your index number you wish to change:");
+							indexNumberTemp6 = scan.nextInt();
+							if(indexNumberTemp6<=0) {
+								System.out.println("Please insert a positive value");
+							}
+						}while(indexNumberTemp6<=0);
+						//swopIndex(indexNumberTemp6, peerUserName, peerHashedPassword, peerIndexNumberTemp);
+						break;
+					case 7: // Exit
+						// Call method to (re)write files
+						System.out.println("You have logged out.");
+						break;
+					default:
+						System.out.println("Please insert a positive integer from 1-7");
+				}
+			}while(inputChoice != 7);
+			
+		}
 	
 	public void addCourse(Student studentLoggedIn,int addIndexNumber) {
 		//verification
