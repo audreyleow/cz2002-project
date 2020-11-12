5c957d29c561ebdf6c06f7be330bf0667a958bda
limsiyi <73398613+limsiyi@users.noreply.github.com>
2020-11-12 16:44:38 +0800

    Update Lesson.java

--- Lesson.java
+++ Lesson.java
@@ -6,5 +6,26 @@ public class Lesson {
 	private String classVenue;
 	private ArrayList<Day> classDay;
 	private ArrayList<int[]> classTiming;
+	private ArrayList<Week> classWeek;
+	
+	enum LessonType {
+		LECTURE;
+		TUTORIAL;
+		LABORATORY;
+	}
+	enum Day {
+		MONDAY;
+		TUESDAY;
+		WEDNESDAY;
+		THURSDAY;
+		FRIDAY;
+		SATURDAY;
+		SUNDAY;
+	}
+	enum Week {
+		EVERY; //every week
+		ODD;
+		EVEN;
+	}
 
 }
