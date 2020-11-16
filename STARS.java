6034584ec5e64552f2a5e71cef0851f35a7129d0
Audrey <audreyleow@yahoo.com.sg>
2020-11-17 01:48:38 +0800

    Update STARS.java

--- STARS.java
+++ STARS.java
@@ -2,17 +2,6 @@ import java.util.ArrayList;
 
 public class STARS {
 	
-	
-	private ArrayList<String> Student = new ArrayList<String>();
-
-	private ArrayList<String> Admin = new ArrayList<String>();
-	
-	private ArrayList<String> Course = new ArrayList<String>();
-	
-	private long studentAccessPeriod;
-	
-	private long studentAccessDuration;
-	
 	public static void checkVacancies(int indexNumber) {
 		//verification
 		if (UniDataBase.verifyClassIndex(indexNumber)==false) {
