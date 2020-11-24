2a79bdab857caeaa6ce7ccadb59f6f3030e52b68
limsiyi <73398613+limsiyi@users.noreply.github.com>
2020-11-24 17:22:13 +0800

    Update ClassIndex.java

--- ClassIndex.java
+++ ClassIndex.java
@@ -1,6 +1,15 @@
 import java.util.ArrayList;
 import java.io.Serializable;
 public class ClassIndex implements Serializable{
+	/**
+	 * Name of course
+	 * Unique code of course
+	 * index number of course
+	 * number of empty slots of the index number
+	 * list of lessons
+	 * list of student on waiting list when index is fully registered
+	 * list of student that are registered in the index
+	 */
 	private String courseName;
 	private String courseCode;
 	private int indexNum;
@@ -9,7 +18,16 @@ public class ClassIndex implements Serializable{
 	private ArrayList<Student> waitList;
 	private ArrayList<Student> studentList;
 	
-	
+	/**
+	 * Constructor for a Class Index of a course
+	 * @param courseName
+	 * @param courseCode
+	 * @param indexNum
+	 * @param classVacancy
+	 * @param lessonsList
+	 * @param waitList
+	 * @param studentList
+	 */
 	public ClassIndex(String courseName, String courseCode, int indexNum, int classVacancy, ArrayList<Lesson> lessonsList, ArrayList<Student> waitList, ArrayList<Student> studentList) {
 		this.courseName = courseName;
 		this.courseCode = courseCode;
@@ -20,50 +38,121 @@ public class ClassIndex implements Serializable{
 		this.studentList = studentList;
 	}
 	
+	/**
+	 * Get the course code for the class index
+	 * @return course code of class index
+	 */
 	public String getCourseCode() {
 		return courseCode;
 	}
-
+	
+	/**
+	 * Set the course code 
+	 * @param courseCode course code 
+	 */
 	public void setCourseCode(String courseCode) {
 		this.courseCode = courseCode;
 	}
-
+	
+	/**
+	 * Get the course name for the class index
+	 * @return name of the course
+	 */
 	public String getCourseName() {
 		return courseName;
 	}
+	
+	/**
+	 * Get the index number 
+	 * @return index number
+	 */
 	public int getIndexNum() {
 		return indexNum;
 	}
+	
+	/**
+	 * Get the number of empty slots for the class
+	 * @return number of empty slots 
+	 */
 	public int getClassVacancy() {
 		return classVacancy;
 	}
+	
+	/**
+	 * Get the list of lessons 
+	 * @return list of lessons
+	 */
 	public ArrayList<Lesson> getLessonsList() {
 		return lessonsList;
 	}
+	
+	/**
+	 * Get the students on the waiting list
+	 * @return students on waiting list
+	 */
 	public ArrayList<Student> getWaitList() {
 		return waitList;
 	}
+	
+	/**
+	 * Get the students who are registered 
+	 * @return students who are registered
+	 */
 	public ArrayList<Student> getStudentList() {
 		return studentList;
 	}
+	
+	/**
+	 * Set the course name 
+	 * @param courseName
+	 */
 	public void setCourseName(String courseName) {
 		this.courseName = courseName;
 	}
+	
+	/**
+	 * Set the index number 
+	 * @param indexNum
+	 */
 	public void setIndexNum(int indexNum) {
 		this.indexNum = indexNum;
 	}
+	
+	/**
+	 * Set the class vacancy for the index
+	 * @param classVacancy
+	 */
 	public void setClassVacancy(int classVacancy) {
 		this.classVacancy = classVacancy;
 	}
+	
+	/**
+	 * set the lessons for the index
+	 * @param lessonsList
+	 */
 	public void setLessonsList(ArrayList<Lesson> lessonsList) {
 		this.lessonsList = lessonsList;
 	}
+	
+	/**
+	 * set the students on waitlist
+	 * @param waitList
+	 */
 	public void setWaitList(ArrayList<Student> waitList) {
 		this.waitList = waitList;
 	}
+	
+	/**
+	 * set the students to register them
+	 * @param studentList
+	 */
 	public void setStudentList(ArrayList<Student> studentList) {
 		this.studentList = studentList;
 	}
+	
+	/**
+	 * Override class index if both of them are equivalent of each other
+	 */
 	@Override
 	public boolean equals(Object other) {
 		 if (other == this) { 
