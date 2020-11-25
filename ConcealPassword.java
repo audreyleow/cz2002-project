b1a97014fccd3cd70b547eb389957d17335a7f7a
Audrey <audreyleow@yahoo.com.sg>
2020-11-25 15:50:53 +0800

    Update ConcealPassword.java

--- ConcealPassword.java
+++ ConcealPassword.java
@@ -4,34 +4,42 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class ConcealPassword {
-	// Handles console input when running outside of Eclipse
-		  public static String getPasswordMasked(Console cons) {
-		    char[] pswd;
-		    while (true) {
-		    	System.out.println("Please input password:");
-		    	pswd = cons.readPassword();
-		      if (pswd != null) {
-		        if (pswd.length > 0) {
-		          return new String(pswd);
-		        } else {
-		          System.out.println("Invalid input\n");
-		        }
-		      }
-		    }
-		  }
+	/**
+	 * Handles console input when running outside of Eclipse
+	 * @param cons
+	 * @return
+	 */
+	
+	  public static String getPasswordMasked(Console cons) {
+	    char[] pswd;
+	    while (true) {
+	    	System.out.println("Please input password:");
+	    	pswd = cons.readPassword();
+	      if (pswd != null) {
+	        if (pswd.length > 0) {
+	          return new String(pswd);
+	        } else {
+	          System.out.println("Invalid input\n");
+	        }
+	      }
+	    }
+	  }
 
-		   // Handles console input when running inside of Eclipse;
-		   // @throws IOException if password is zero-length
-		  public static String getPasswordWithinEclipse() throws IOException {
-		    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		    System.out.println("Please input password:");
-		    String password = reader.readLine();
-		    if (password != null) {
-		      if (password.length() <= 0) {
-		        System.out.println("Invalid input\n");
-		        throw new IOException("Error reading in password");
-		      }
-		    }
-		    return password;
-		  }
+	  /**
+	   * Handles console input when running inside of Eclipse;
+	   * @return
+	   * @throws IOException if password is zero-length
+	   */
+	  public static String getPasswordWithinEclipse() throws IOException {
+	    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	    System.out.println("Please input password:");
+	    String password = reader.readLine();
+	    if (password != null) {
+	      if (password.length() <= 0) {
+	        System.out.println("Invalid input\n");
+	        throw new IOException("Error reading in password");
+	      }
+	    }
+	    return password;
+	  }
 }
