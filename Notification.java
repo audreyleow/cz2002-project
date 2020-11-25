c48a27eac4caacc0ca589b61c7ea1f83bd5be228
Audrey <audreyleow@yahoo.com.sg>
2020-11-25 16:30:44 +0800

    Update Notification.java

--- Notification.java
+++ Notification.java
@@ -1,4 +1,9 @@
-
 public interface Notification {
+	/**
+	 * Creates an abstract method for sending notification to students
+	 * @param student
+	 * @param messageSubject
+	 * @param messageContent
+	 */
 	public void sendNotification(Student student,String messageSubject,String messageContent);
 }
